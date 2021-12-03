83bba8410f91a7d579647e56c9733a031f0210fb
fortunateowl <92759162+fortunateowl@users.noreply.github.com>
2021-12-03 15:36:31 -0800

    Add files via upload
    simple web scrape code in python

---
+++ Simple_Scrape.py
@@ -0,0 +1,36 @@
+# Call the necessary packages for the program
+import requests 
+from bs4 import BeautifulSoup
+
+# get the Wikipedia homepage and assign it to the variable "response"
+url = "https://www.wikipedia.org/"
+response = requests.get(url)
+
+# assign the content of the webpage to the variable "content"
+content = response.content
+
+# call BeautifulSoup to parse the information on the webpage.
+soup = BeautifulSoup(content, "lxml")
+
+# Create a variable "links," loop through the the content with an "a" tag
+# to find links and assign the links to the variable.
+all_a = soup.find_all("a")
+links = []
+for i in all_a:
+    if i.get("href") != '#':
+        links.append(i.get("href"))
+ 
+    
+
+with open('urls.txt', 'w') as file:
+      file_content = "\n".join(links)
+ 
+      file.write(file_content)
+
+
+file.close()
+
+# Print and count the links
+print (*links, sep = "\n")
+print ("\n \nThe number of links on the Wikipedia homepage is: ", len(links))
+
